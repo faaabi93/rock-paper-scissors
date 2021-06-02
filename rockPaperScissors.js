8bd3266bd5dc56dda35d171cd32406ab5c00903d
faaabi93 <fabian.baiersdoerfer@gmx.de>
2021-06-02 08:53:56 +0200

    added files

---
+++ rockPaperScissors.js
@@ -0,0 +1,40 @@
+console.log("Hallo Welt!");
+
+function computerPlay() {
+    var arr = ["Rock", "Paper", "Scissors"];
+    var rand = Math.floor(Math.random() * 3);
+    return arr[rand];
+}
+
+function playRound(playerSelection, computerSelection) {
+    var playerSelection = playerSelection.toLowerCase();
+    var computerSelection = computerSelection.toLowerCase();
+
+    /*console.log(playerSelection);
+    console.log(computerSelection);*/
+
+    if (playerSelection === computerSelection) {
+        return "Draw!";
+    }
+
+    if (
+        (playerSelection === "rock" && computerSelection === "scissors") || 
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+        ) {
+            return `You won! ${playerSelection} beats ${computerSelection}`;
+        }
+    
+    return `You lost! ${computerSelection} beats ${playerSelection}`;
+}
+
+function game() {
+    for (var i = 0; i < 5; i++) {
+        var computerSelection = computerPlay();
+        var playerSelection = window.prompt("Enter your selection!");
+
+        console.log(playRound(computerSelection, playerSelection));
+    }
+}
+
+game()
